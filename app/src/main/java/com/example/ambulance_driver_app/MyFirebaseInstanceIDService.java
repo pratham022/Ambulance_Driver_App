492e6922c527a1901f804523181c2ec6a65f6d2a
pratham022 <pthombre657@gmail.com>
2021-06-05 00:29:57 +0530

    Started with Push Notifications

---
+++ app/src/main/java/com/example/ambulance_driver_app/MyFirebaseInstanceIDService.java
@@ -0,0 +1,35 @@
+package com.example.ambulance_driver_app;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Belal on 03/11/16.
+ */
+
+
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        //calling the method store token and passing token
+        storeToken(refreshedToken);
+    }
+
+    private void storeToken(String token) {
+        //we will save the token in sharedpreferences later
+
+    }
+}
